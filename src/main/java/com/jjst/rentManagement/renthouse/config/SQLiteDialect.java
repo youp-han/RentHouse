a33a7b6505a92cbf0191db0471e82725962c45f7
han.sungyoup <youp.han@gmail.com>
2025-07-22 10:04:10 +0900

    added untrakced files

---
+++ src/main/java/com/jjst/rentManagement/renthouse/config/SQLiteDialect.java
@@ -0,0 +1,9 @@
+//package com.jjst.rentManagement.renthouse.config;
+//
+//import org.hibernate.dialect.Dialect;
+//
+//public class SQLiteDialect extends Dialect {
+//    public SQLiteDialect() {
+//        // SQLite에 필요한 설정 추가
+//    }
+//}
