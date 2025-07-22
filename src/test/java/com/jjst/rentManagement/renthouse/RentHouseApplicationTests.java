4a3b2a35e03b5f596a0b26c30a87696f813988fd
han.sungyoup <youp.han@gmail.com>
2025-07-22 10:50:11 +0900

    added test properties

--- src/test/java/com/jjst/rentManagement/renthouse/RentHouseApplicationTests.java
+++ src/test/java/com/jjst/rentManagement/renthouse/RentHouseApplicationTests.java
@@ -1,8 +1,11 @@
 package com.jjst.rentManagement.renthouse;
 
+import org.springframework.test.context.ActiveProfiles;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@ActiveProfiles("local")
 @SpringBootTest
 class RentHouseApplicationTests {
 
